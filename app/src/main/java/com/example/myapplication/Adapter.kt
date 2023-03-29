5bbec884df9f3919483f73947959f413a6104cfe
Acer <abdujafar.rashidov.2006@gmail.com>
2023-03-29 21:32:18 +0500

    Last_Animation

--- app/src/main/java/com/example/myapplication/Adapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter.kt
@@ -4,23 +4,23 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.animation.AnimationUtils
+import java.util.Collections
 
-class Adapter(var context: Context, var foods: MutableList<Data>): RecyclerView.Adapter<Adapter.MyHolder>() {
+class Adapter(var context: Context, var foods: MutableList<Data>):
+    RecyclerView.Adapter<Adapter.MyHolder>(), ItemTouchHelperAdapter {
 
     class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val img: ImageView = itemView.findViewById(R.id.img)
         val name: TextView = itemView.findViewById(R.id.name)
         val receipe: TextView = itemView.findViewById(R.id.recepie)
-        val main : MenuView.ItemView = itemView.findViewById(R.id.cooks)
+
 
 
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
         return MyHolder(LayoutInflater.from(parent.context).inflate(R.layout.activity_food_detail, parent, false))
     }
@@ -36,7 +36,27 @@ class Adapter(var context: Context, var foods: MutableList<Data>): RecyclerView.
         holder.name.text = food.name
         holder.receipe.text = food.receipes
 
-        val anim = AnimationUtils.loadAnimation(context, R.anim.itemanim)
+        val anim = AnimationUtils.loadAnimation(holder.itemView.context, R.anim.itemanim)
         holder.itemView.startAnimation(anim)
     }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int) {
+        if(fromPosition<toPosition){
+            for (i in fromPosition until toPosition){
+                Collections.swap(foods,i , i+1)
+            }
+        }else{
+            for (i in fromPosition downTo  toPosition+1){
+                Collections.swap(foods,i , i-1)
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition)
+    }
+
+    override fun onItemDismiss(position: Int) {
+        foods.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -37,7 +38,36 @@ class MainActivity : AppCompatActivity() {
 
             count++
         }
+
         val adapter = Adapter(this,foods)
+
+        val touchHelper = object : ItemTouchHelper.Callback(){
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+                val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+                return makeMovementFlags(dragFlags,swipeFlags)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                adapter.onItemMove(viewHolder.adapterPosition, target.adapterPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                adapter.onItemDismiss(viewHolder.adapterPosition)
+            }
+
+        }
+
+        val itemTouchHelper = ItemTouchHelper(touchHelper)
+        itemTouchHelper.attachToRecyclerView(binding.rv)
         binding.rv.adapter = adapter
         newRecycrerview=binding.rv
         newRecycrerview.layoutManager=LinearLayoutManager(this)
